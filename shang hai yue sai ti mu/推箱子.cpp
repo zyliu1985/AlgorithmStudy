bb994608a29c083e562afed972fcf02e2f33e28e
jim <jim@study.com>
2023-11-25 21:44:09 +0800

    提交

--- shang hai yue sai ti mu/推箱子.cpp
+++ shang hai yue sai ti mu/推箱子.cpp
@@ -2,27 +2,23 @@
 #include <cmath>
 using namespace std;
 
+int a[1000005];
 int main(){
 	int t, n;
+	string s;
 	cin >> t;
-	for (int i = 1; i <= t; i++){
-		int ans = 1e8;
+	for (int _ = 1; _ <= t; _++){
 		cin >> n;
-		string s;
 		cin >> s;
 		int cnt = 0;
-		for (int j = 0; j < s.size(); j++){
-			if (s[j] == '@') cnt++;
-		}
-		for (int j = 0; j < s.size()-cnt; j++){
-			int x = 0, y = 0;
-			for (int k = 0; k < s.size(); k++){
-				if (s[k] == '@'){
-					x += abs(j+y-k);
-					y++;
-				}
-			}
-			ans = min(ans, x);
+		for (int i = 0; i < s.size(); i++) // 记录每个箱子的位置
+			if (s[i] == '@') a[++cnt] = i;
+		for (int i = 1; i <= cnt; i++) // 位置偏移成相对值
+			a[i] -= i;
+		int mid = cnt/2+1; // 中位数（偶数偏右）或基准数
+		int ans = 0;
+		for (int i = 1; i <= cnt; i++){
+			ans += abs(a[i] - a[mid]);
 		}
 		cout << ans << endl;
 	}
--- shang hai yue sai ti mu/桌式足球.cpp
+++ shang hai yue sai ti mu/桌式足球.cpp
@@ -1,24 +1,87 @@
 #include <iostream>
-#include <queue>
+#include <cmath>
 using namespace std;
 
-int m[200005];
-queue<int> q[200005];
-bool vis[200005]
+int q[200005];
+//坐标标记
+bool box[200005];
+int c[200005];
+
+//queue<int> q[200005];
+bool vis[200005];
 bool flag[200005];
 int n, m;
 int bfs(){
 	
 }
+
+// 返回差值的和
+int updateC() {
+	int d = 0;
+	int mi = 9999999;
+	// 计算离小球最近的洞之间的距离
+	for (int i = 1; i <= n; i++) {
+		if(vis[i]) continue;
+//		if(box[q[i] + m]) vis[i]=true;
+		int count = 1;
+		while(true) {
+			if(box[q[i] + m + count]) {
+				c[i] = count;
+				if(abs(c[i]) <= mi) {
+					mi = abs(c[i]);
+					d = -1;
+				}
+				break;
+			} else if(box[q[i] + m - count]) {
+				c[i] = -count;
+				if(abs(c[i]) <= mi) {
+					mi = abs(c[i]);
+					d = 1;
+				}
+				break;
+			} else {
+				count++;
+			}
+		}
+	}
+	
+//	for (int i = 1; i <= n; i++) {
+//		cout << c[i] << ' ';
+//	}
+//	cout << d << endl;
+	return d;
+}
+
 int main(){
 	cin >> n >> m;
-	for (int i = 1; i <= n; i++){
-		int x;
-		cin >> x;
-		q[i].push(x);
+	for (int i = 1; i <= n; i++) cin>>q[i];
+		
+	for (int i = 1; i <= m; i++) {
+		int p;
+		cin >> p;
+		// 统一往右移动m，避免出现负数
+		box[p + m] = true;
 	}
-	for (int i = 1; i <= m; i++) cin >> m[i];
 	
+	int ans = 0;
+	int sum = updateC();
+	while(sum != 0) {
+		ans++;
+		if(sum < 0) {
+			for (int i = 1; i <= n; i++) {
+				q[i] += 1;
+				if(q[i] == 0) vis[i] = true;
+			}
+			sum = updateC();
+		} else {
+			for (int i = 1; i <= n; i++) {
+				q[i] -= 1;
+				if(q[i] == 0) vis[i] = true;
+			}
+			sum = updateC();
+		}
+	}
 	
+	cout << ans << endl;
 	return 0;
 }
